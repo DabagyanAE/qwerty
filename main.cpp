64870b8aad5db498252acf88e01ffbcf7bfc196f
u211-13 <u211-13@public.mpei.local>
2018-04-05 15:03:56 +0300

    Первый

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    cout << "Enter A & B";
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n';
+    return 0;
+}
